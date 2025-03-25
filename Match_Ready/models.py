ce7a202acc2030bef3d1155194816e2dc78ba409
“David <2854863O@student.gla.ac.uk>
2025-03-25 16:32:52 +0000

    user model changed

--- Match_Ready/models.py
+++ Match_Ready/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class User(models.Model):
-    user_id = models.CharField(max_length=128, unique=True)
-    name = models.CharField(max_length=128)
+    user_name = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
