fe25d843a6ae6a40da542ddc7b4d4017ea45b3b7
“David <2854863O@student.gla.ac.uk>
2025-03-25 17:05:42 +0000

    settings update

--- Match_Ready/models.py
+++ Match_Ready/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class User(models.Model):
     user_name = models.CharField(max_length=128, unique=True)
--- Match_Ready/views.py
+++ Match_Ready/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
 from datetime import datetime
 
 
