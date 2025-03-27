8e83d916102f23b5971cbd338f3d0ff7068fbd64
PikaQ1U <2903477Q@student.gla.ac.uk>
2025-03-27 22:44:34 +0000

    ajax and test

--- Match_Ready/urls.py
+++ Match_Ready/urls.py
@@ -1,25 +1,23 @@
 from django.urls import path
 from Match_Ready import views
-from django.urls import include
-from django.contrib import admin 
-
-
+# Removed unused imports: include, admin
 
 app_name = 'Match_Ready'
 
 urlpatterns = [
     path('', views.index, name ='index'),
     path('about/', views.about, name='about'),
-    path('fixtues/', views.fixtures, name='fixtures'),
+    path('contact/', views.contact, name='contact'),
+    path('fixtures/', views.fixtures, name='fixtures'),
+    path('signup/', views.user_register, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('create_team/', views.create_team, name='create_team'),
     path('find_team/', views.find_team, name='find_team'),
-    path('login/', views.user_login, name='login'),
-    path('make_team/', views.make_team, name='make_team'),
-    path('upcoming_matches/', views.upcoming_matches, name='upcoming_matches'),
     path('my_team/', views.my_team, name='my_team'),
-    path('signup/', views.user_register, name='signup'),
-    path('player_list/', views.player_list, name='player_list'),
+    path('my_team/players/', views.player_list, name='player_list'),
     path('add_match/', views.add_match, name='add_match' ),
-    path('logout/', views.user_logout, name='logout'),
-    path('contact/', views.contact, name='contact'),
+    path('upcoming_matches/', views.upcoming_matches, name='upcoming_matches'),
+    path('attend_match_ajax/', views.attend_match_ajax, name='attend_match_ajax'),
+    path('make_team/', views.make_team, name='make_team'),
 ]
